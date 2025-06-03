6870e3cc3081e6654e5bee218e970b16c745297c
Pessina <fs.pessina@gmail.com>
2025-06-03 22:54:57 +0200

    Clean up code

--- zk/sp1-solana/example/script/src/main.rs
+++ zk/sp1-solana/example/script/src/main.rs
@@ -54,19 +54,13 @@ async fn run_verify_instruction(groth16_proof: SP1Groth16Proof) {
 
 #[tokio::main]
 async fn main() {
-    // Setup logging for the application.
     sp1_sdk::utils::setup_logger();
-
-    let proof_file = "../../proofs/fibonacci_proof.bin";
-
-    // Setup the prover client.
+    
     let client = ProverClient::from_env();
 
-    // Setup the inputs.
     let mut stdin = SP1Stdin::new();
     stdin.write(&20u32);
-
-    // Setup the program for proving.
+    
     let (pk, vk) = client.setup(ELF);
 
     println!(
@@ -75,28 +69,18 @@ async fn main() {
     );
 
     // Generate a Groth16 proof
-    let groth16_proof_result = client
+    let mut proof = client
         .prove(&pk, &stdin)
         .groth16()
         .run()
         .expect("Groth16 proof generation failed");
 
-    // Save the generated proof to `proof_file`.
-    groth16_proof_result.save(&proof_file).unwrap();
-
-    // Load the proof from the file, and convert it to a Borsh-serializable `SP1Groth16Proof`.
-    let mut sp1_proof_with_public_values = SP1ProofWithPublicValues::load(&proof_file).unwrap();
-
     // Remove the TEE proof from the proof as it's not compatible with the sp1_solana crate, this repo initially used the 4.x.x version of the sp1
-    sp1_proof_with_public_values.tee_proof = None;
-
-    if let Some(_tee_proof) = sp1_proof_with_public_values.tee_proof.clone() {
-        panic!("TEE in proof abort");
-    };
-
+    proof.tee_proof = None;
+    
     let groth16_proof = SP1Groth16Proof {
-        proof: sp1_proof_with_public_values.bytes(),
-        sp1_public_inputs: sp1_proof_with_public_values.public_values.to_vec(),
+        proof: proof.bytes(),
+        sp1_public_inputs: proof.public_values.to_vec(),
     };
 
     // Send the proof to the contract, and verify it on `solana-program-test`.
