ec3b8d072a9738ef8b88c269b2dcdcd0f05886e2
Felipe Pessina <fpessina@chorus.one>
2026-02-04 15:34:04 +0700

    Cleanup

--- ton-pool-contracts/tests/shared/shared.ts
+++
@@ -1,9 +0,0 @@
-// Operation constants used when interacting with the TonPool contract.
-export const acceptDepositOperation = 2577928699;
-export const acceptWithdrawOperation = 2711607604;
-export const bounceOperation = 0xffffffff;
-export const depositOperation = 2077040623;
-export const depositResponse = 3326208306;
-export const withdrawOperation = 3665837821;
-export const withdrawResponseDelayed = 1958425639;
-export const withdrawResponseImmediate = 601104865;
--- ton-pool-contracts/tests/wrappers/TonPool.compile.ts
+++
@@ -1,7 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-import { join } from 'path';
-
-export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: [join(__dirname, '../../nominators.fc')],
-};
